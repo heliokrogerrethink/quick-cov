e1a9a12b3e13c8de914c56f992cd82b92e2134c4
Hélio Kroger <heliokroger@Helios-MacBook-Pro.local>
2021-07-29 11:00:06 -0300

    adding file-helpers test

--- src/file-helpers.test.ts
+++ src/file-helpers.test.ts
@@ -11,27 +11,27 @@ describe('testing file helpers', () => {
         expect(result).toBe(hash);
     });
 
-    // it('getTestFilesChecksums should return the hashes from a list of paths', () => {
-    //     const testFiles = ['./file-1.js', './file-2.js'];
-    //     const contents = ['file-1-content', 'file-2-content'];
-    //     const hashes = {
-    //         './file-1.js': {
-    //             hash: '6ab9727ceca4d24f887fdf03b666cb720e5a7e2e6f36c45de5c224cee55292c3',
-    //         },
-    //         './file-2.js': {
-    //             hash: 'be08233ca5d48a654ec15e71889ab813bcd41f74d54a3a0dadb29b213420fc2b',
-    //         },
-    //     };
+    it('getTestFilesChecksums should return the hashes from a list of paths', () => {
+        const testFiles = ['./file-1.js', './file-2.js'];
+        const contents = ['file-1-content', 'file-2-content'];
+        const hashes = {
+            './file-1.js': {
+                hash: '6ab9727ceca4d24f887fdf03b666cb720e5a7e2e6f36c45de5c224cee55292c3',
+            },
+            './file-2.js': {
+                hash: 'be08233ca5d48a654ec15e71889ab813bcd41f74d54a3a0dadb29b213420fc2b',
+            },
+        };
 
-    //     jest.spyOn(fs, 'readFileSync').mockImplementation(
-    //         (path: PathOrFileDescriptor) => {
-    //             const index = testFiles.indexOf(path as string);
-    //             return contents[index];
-    //         }
-    //     );
+        jest.spyOn(fs, 'readFileSync').mockImplementation(
+            (path: PathOrFileDescriptor) => {
+                const index = testFiles.indexOf(path as string);
+                return contents[index];
+            }
+        );
 
-    //     const result = getFilesChecksums(testFiles);
+        const result = getFilesChecksums(testFiles);
 
-    //     expect(result).toEqual(hashes);
-    // });
+        expect(result).toEqual(hashes);
+    });
 });
